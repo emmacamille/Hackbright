7e725a288e8625bdab45dc657c9de4114021053c
Gulmira <miramaxeast@yahoo.com>
2016-01-26 20:21:27 -0800

    Classwork 1/26/21

---
+++ functions_practice.py
@@ -0,0 +1,5 @@
+def hello_name(name):
+	print "Hello",name
+partner_name = "Emma"
+hello_name(partner_name)
+print hello_name
---
+++ intro_class/calculator.py
@@ -0,0 +1,27 @@
+def add_num(num1, num2):
+	total = num1 + num2
+	return total
+
+def subtract_num(num1, num2):
+	difference = num1 - num2
+	return difference
+
+def multiply_num(num1,num2):
+	total_mult = num1 * num2
+	return total_mult
+
+def divide_num(num1,num2):
+	divided = num1/num2
+	return divided
+
+def modulo(num1,num2):
+	remainder = num1%num2
+	return remainder
+
+def power(num1,num2):
+	to_the_power = num1**num2
+	return to_the_power
+
+def num_squared(num1):
+	squared = num1**2
+	return squared

8397d5c92d5c831923bc50b60712ac90fe17b182
Gulmira <miramaxeast@yahoo.com>
2016-01-28 20:53:24 -0800

    Classwork Jan28

---
+++ intro_class/conditionals.py
@@ -0,0 +1,27 @@
+age = 23
+if age >= 18:
+	print "Yay I can vote!"
+else:
+	print "Aww, I cannot vote :("
+
+if age >= 18 or age <= 21:
+	print "I can vote and go to a bar!"
+else:
+	print " I can vote but cannot go to a bar :("
+
+if 8 / 2: 
+	print "The number 8 is even."
+else: 
+	print "The number 8 is odd."	
+
+
+if 8 % 2: 
+	print "The number 8 is odd."
+else: 
+	print "The number 8 is even."		
+
+if 8 % 2 and 9 %2:
+	print "Both numbers are even."
+else:
+	print "Those numbers aren't even."
+

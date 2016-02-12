97241c6ee5a64d6644fa717c285d1d92ee9abddf
EMMA SCHACTER <emmacamille@EMMAs-MacBook-Air-2.local>
2016-02-11 18:56:26 -0800

    Revised for unlimited keys

--- codeapp_211.py
+++ codeapp_211.py
@@ -1,17 +1,16 @@
 food_price_tuple = ('sushi', 7.50, 'burrito', 8.20, 'cheeseburger', 6.00, 'hot dog', 3.40, 'fried rice', 9.99)
 
-food_dictionary = {}
 
-def tuple_to_dictionary (tup):
-	food_dictionary[tup[0]] = (tup[1])
-	food_dictionary[tup[2]] = (tup[3])
-	food_dictionary[tup[4]] = (tup[5])
-	food_dictionary[tup[6]] = (tup[7])
-	food_dictionary[tup[8]] = (tup[9])
+def tuple_to_dictionary (tuple):
+	item_index = 0
+	food_dictionary = {}
+	for item in tuple:
+		if item_index % 2 == 0:
+			food_dictionary[item] = tuple[item_index + 1]
+		item_index += 1
 	return food_dictionary
 
+food_dictionary = tuple_to_dictionary(food_price_tuple)
 
-tuple_to_dictionary(food_price_tuple)
-
-print food_dictionary['sushi']
 print food_dictionary
+print food_dictionary["sushi"]

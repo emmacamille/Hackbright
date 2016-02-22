5cc5b7eb1f103b85246918baef5e7d129383df85
EMMA SCHACTER <emmacamille@EMMAs-MacBook-Air-2.local>
2016-02-21 21:19:39 -0800

    Final work

--- MidSessQuiz/midsess_dict.py
+++ MidSessQuiz/midsess_dict.py
@@ -1,21 +1,50 @@
-text_file_fo = open("testText.txt")
-text_file_list = text_file_fo.readlines()
+
+# count letters in file
+# not the prettiest, but it works!
+
+text_file_fo = open("one_fish.txt")
+text_file_list = text_file_fo.read()
 text_file_fo.close()
 
-text_file_list = text_file_list.split("\n")
+def letters_to_count(file_for_count):
+	count_dict = {}
+	file_for_count = file_for_count.replace("\n"," ")
+	file_for_count = file_for_count.replace(",", "")
+	file_for_count = file_for_count.replace(".", "")
+	file_for_count = file_for_count.replace("!", "")
+	file_for_count = file_for_count.replace("?", "")
+	file_for_count = file_for_count.replace(" ", "")
+	file_for_count = file_for_count.replace("\xe2\x80\x99t","")
+		# removes all punctionation. Line 12 is apostrophe.
+	file_for_count = file_for_count.lower()
+	for i in file_for_count:
+		if (i not in count_dict):
+			count_dict[i] = 1
+		else:
+			count_dict[i] += 1
+	return count_dict
 
-print text_file_list
+print letters_to_count(text_file_list)
 
-count_dict = {}
+# count words in file
 
-# text_file_list = text_file_list.upper()
+def words_to_count(file_for_count):
+	word_count_dict = {}
+	file_for_count = file_for_count.replace("\n"," ")
+	file_for_count = file_for_count.replace(",", "")
+	file_for_count = file_for_count.replace(".", "")
+	file_for_count = file_for_count.replace("!", "")
+	file_for_count = file_for_count.replace("?", "")
+	file_for_count = file_for_count.replace("\xe2\x80\x99t","")
+		# removes all punctionation. Line 12 is apostrophe.
+	file_for_count = file_for_count.lower()
+	file_for_count = file_for_count.split(" ")
+	for i in file_for_count:
+		if (i not in word_count_dict):
+			word_count_dict[i] = 1
+		else:
+			word_count_dict[i] += 1
+	return word_count_dict
 
-# def count_letters():
-# for i in text_file_list:
-# 	if (i not in count_dict):
-# 		count_dict[i] = 1
-# 	else:
-# 		count_dict[i] += 1
-# 	# return count_dict
+print words_to_count(text_file_list)
 
-# print count_dict
--- MidSessQuiz/midsess_loops.py
+++ MidSessQuiz/midsess_loops.py
@@ -5,11 +5,30 @@
 # print draw_square(8)
 
 def draw_checkerboard():
-	for i in range(64):
-		if i % 2:
-			print "o",
-		else:
-			print "x",
+	counter = 1
+
+	for i in range(1, 65):
+		if (counter % 2 != 0) : # odd rows start with "x"
+			#print (i % 2),
+			if (i % 2 == 0):
+				print "o",
+			else:
+				print "x",
+			#print (i % 8),
+			if (i % 8 == 0):
+				print "\n"
+				counter += 1
+
+		else: # row starts with "o"
+			#print (i % 2),
+			if (i % 2 == 0):
+				print "x",
+			else:
+				print "o",
+			#print (i % 8),
+			if (i % 8 == 0):
+				print "\n"
+				counter += 1
 	
 
 print draw_checkerboard()
